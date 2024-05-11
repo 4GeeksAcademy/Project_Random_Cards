8eac9422f67e53d9cde575717e9425705c477158
MariaVictoriaZ <163335173+MariaVictoriaZ@users.noreply.github.com>
2024-05-11 15:40:24 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,43 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
+  console.log("hello");
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // document.querySelector(".card").classList.add(generateRandomSuit());
+  // document.querySelector(".card").innerHTML = generateRandomNumber();
+
+  let generateRandomSuit = () => {
+    let suit = ["♦", "♥", "♠", "♣"];
+    let indexSuit = Math.floor(Math.random() * suit.length);
+    console.log(suit[indexSuit]);
+    return suit[indexSuit];
+  };
+  generateRandomSuit();
+
+  let randomSuit = generateRandomSuit();
+  document.querySelector(".top").textContent = randomSuit;
+  document.querySelector(".bottom").textContent = randomSuit;
+  let generateRandomNumber = () => {
+    let number = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexNumber = Math.floor(Math.random() * number.length);
+    return number[indexNumber];
+  };
+
+  let randomNumber = generateRandomNumber();
+  document.querySelector(".number").textContent = randomNumber;
 };
